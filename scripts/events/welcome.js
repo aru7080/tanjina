ad937389d6c494dee8831af0f6eaefb69560d14d
aru7080 <arutanjina1@gmail.com>
2025-05-22 07:26:09 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,12 +1,11 @@
 const { getTime, drive } = global.utils;
-if (!global.temp.welcomeEvent)
-  global.temp.welcomeEvent = {};
+if (!global.temp.welcomeEvent) global.temp.welcomeEvent = {};
 
 module.exports = {
   config: {
     name: "welcome",
-    version: "2.0",
-    author: "NTKhang ",
+    version: "2.1",
+    author: "NTKhang x @ARIYAN",
     category: "events"
   },
 
@@ -19,31 +18,24 @@ module.exports = {
       multiple1: "you",
       multiple2: "you guys",
       defaultWelcomeMessage:
-`🥰 𝙰𝚂𝚂𝙰𝙻𝙰𝙼𝚄𝙰𝙻𝙰𝙸𝙺𝚄𝙼 {userNameTag}, 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 {multiple} 𝚃𝚘 𝙾𝚞𝚛 『{boxName}』 𝙶𝚛𝚘𝚞𝚙😊
-• 𝙸 𝙷𝚘𝚙𝚎 𝚈𝚘𝚞 𝚆𝚒𝚕𝚕 𝙵𝚘𝚕𝚕𝚘𝚠 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝚁𝚞𝚕𝚎𝚜
-• {prefix}rules 𝚏𝚘𝚛 𝙶𝚛𝚘𝚞𝚙 𝚁𝚞𝚕𝚎𝚜
-• {prefix}help 𝙵𝚘𝚛 𝙰𝚕𝚕 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜
+`🥰 𝙰𝚂𝚂𝙰𝙻𝙰𝙼𝚄𝙰𝙻𝙰𝙸𝙺𝚄𝙼 {userNameTag}, 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 {multiple} 𝚃𝚘 𝙾𝚞𝚛 {boxName} 𝙶𝚛𝚘𝚞𝚙😊
+• 𝙸 𝙷𝚘𝚙𝚎 𝚈𝚘𝚞 𝚆𝚒𝚕𝚕 𝚏𝚘𝚕𝚕𝚘𝚠 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝚁𝚞𝚕𝚎𝚜
+• {prefix}rules 𝚏𝚘𝚛 𝙶𝚛𝚘𝚞𝚙 𝚛𝚞𝚕𝚎𝚜
+• {prefix}help 𝙵𝚘𝚛 𝚊𝚕𝚕 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜
 
 • 𝚈𝚘𝚞 𝙰𝚛𝚎 𝚃𝚑𝚎 {memberIndex} 𝙼𝚎𝚖𝚋𝚎𝚛{memberPlural} 𝚒𝚗 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙
-• 𝙰𝚍𝚍𝚎𝚍 𝙱𝚢: {inviterName}`
+• 𝙰𝚍𝚍𝚎𝚍 𝙱𝚢: {inviterTag}`
     }
   },
 
   onStart: async ({ threadsData, message, event, api, getLang }) => {
     if (event.logMessageType !== "log:subscribe") return;
 
-    const hours = getTime("HH");
     const { threadID } = event;
-    const { nickNameBot } = global.GoatBot.config;
     const prefix = global.utils.getPrefix(threadID);
-    const dataAddedParticipants = event.logMessageData.addedParticipants;
+    const addedParticipants = event.logMessageData.addedParticipants;
 
-    // Bot was added
-    if (dataAddedParticipants.some(u => u.userFbId == api.getCurrentUserID())) {
-      if (nickNameBot)
-        api.changeNickname(nickNameBot, threadID, api.getCurrentUserID());
-      return message.send(getLang("welcomeMessage", prefix));
-    }
+    if (addedParticipants.some(u => u.userFbId == api.getCurrentUserID())) return;
 
     if (!global.temp.welcomeEvent[threadID])
       global.temp.welcomeEvent[threadID] = {
@@ -51,16 +43,16 @@ module.exports = {
         dataAddedParticipants: []
       };
 
-    global.temp.welcomeEvent[threadID].dataAddedParticipants.push(...dataAddedParticipants);
+    global.temp.welcomeEvent[threadID].dataAddedParticipants.push(...addedParticipants);
     clearTimeout(global.temp.welcomeEvent[threadID].joinTimeout);
 
-    global.temp.welcomeEvent[threadID].joinTimeout = setTimeout(async function () {
+    global.temp.welcomeEvent[threadID].joinTimeout = setTimeout(async () => {
       const threadData = await threadsData.get(threadID);
       if (threadData.settings.sendWelcomeMessage === false) return;
 
       const addedUsers = global.temp.welcomeEvent[threadID].dataAddedParticipants;
-      const dataBanned = threadData.data.banned_ban || [];
       const threadName = threadData.threadName;
+      const dataBanned = threadData.data.banned_ban || [];
       const mentions = [];
       const names = [];
 
@@ -76,26 +68,37 @@ module.exports = {
       const memberInfo = await api.getThreadInfo(threadID);
       const memberCount = memberInfo.participantIDs.length;
 
-      // Generate member positions
       const memberIndexList = [];
       for (let i = memberCount - names.length + 1; i <= memberCount; i++) {
         memberIndexList.push(i + getNumberSuffix(i));
       }
 
-const inviterName =await usersData.getName(event.author)
+      const inviterID = event.logMessageData.inviterID;
+      let inviterName = "Unknown";
+      try {
+        const info = await api.getUserInfo(inviterID);
+        inviterName = info[inviterID]?.name || "Unknown";
+      } catch (e) {}
+
+      const inviterTag = {
+        tag: inviterName,
+        id: inviterID
+      };
+      mentions.push(inviterTag);
+
       const form = {
         body: welcomeMsgTemplate
-          .replace(/\{userNameTag\}/g, names.map(n => n).join(", "))
+          .replace(/\{userNameTag\}/g, names.join(", "))
           .replace(/\{multiple\}/g, names.length > 1 ? getLang("multiple2") : getLang("multiple1"))
           .replace(/\{boxName\}/g, threadName)
           .replace(/\{memberIndex\}/g, memberIndexList.join(", "))
           .replace(/\{memberPlural\}/g, names.length > 1 ? "s" : "")
           .replace(/\{inviterName\}/g, inviterName)
+          .replace(/\{inviterTag\}/g, inviterTag.tag)
           .replace(/\{prefix\}/g, prefix),
         mentions
       };
 
-      // Include attachments if any
       if (threadData.data.welcomeAttachment) {
         const files = threadData.data.welcomeAttachment;
         const attachments = files.map(file => drive.getFile(file, "stream"));
@@ -110,9 +113,8 @@ const inviterName =await usersData.getName(event.author)
   }
 };
 
-// Helper to get suffix like 1st, 2nd, 3rd, etc.
 function getNumberSuffix(n) {
   const s = ["th", "st", "nd", "rd"];
   const v = n % 100;
-  return (s[(v - 20) % 10] || s[v] || s[0]);
+  return s[(v - 20) % 10] || s[v] || s[0];
 }
