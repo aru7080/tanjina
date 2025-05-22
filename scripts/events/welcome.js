f9fb2bfa764f384b08f3edb8fcde620314c1d28a
aru7080 <arutanjina1@gmail.com>
2025-05-22 07:50:56 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,120 +1,93 @@
-const { getTime, drive } = global.utils;
-if (!global.temp.welcomeEvent) global.temp.welcomeEvent = {};
+const { getTime, drive } = global.utils; if (!global.temp.welcomeEvent) global.temp.welcomeEvent = {};
 
-module.exports = {
-  config: {
-    name: "welcome",
-    version: "2.1",
-    author: "NTKhang x @ARIYAN",
-    category: "events"
-  },
+module.exports = { config: { name: "welcome", version: "2.1", author: "NTKhang + customized by ChatGPT", category: "events" },
 
-  langs: {
-    en: {
-      session1: "morning",
-      session2: "noon",
-      session3: "afternoon",
-      session4: "evening",
-      multiple1: "you",
-      multiple2: "you guys",
-      defaultWelcomeMessage:
-`🥰 𝙰𝚂𝚂𝙰𝙻𝙰𝙼𝚄𝙰𝙻𝙰𝙸𝙺𝚄𝙼 {userNameTag}, 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 {multiple} 𝚃𝚘 𝙾𝚞𝚛 {boxName} 𝙶𝚛𝚘𝚞𝚙😊
-• 𝙸 𝙷𝚘𝚙𝚎 𝚈𝚘𝚞 𝚆𝚒𝚕𝚕 𝚏𝚘𝚕𝚕𝚘𝚠 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝚁𝚞𝚕𝚎𝚜
-• {prefix}rules 𝚏𝚘𝚛 𝙶𝚛𝚘𝚞𝚙 𝚛𝚞𝚕𝚎𝚜
-• {prefix}help 𝙵𝚘𝚛 𝚊𝚕𝚕 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜
+langs: { en: { session1: "morning", session2: "noon", session3: "afternoon", session4: "evening", multiple1: "you", multiple2: "you guys", defaultWelcomeMessage: `🥰 𝙰𝚂𝚂𝙰𝙻𝙰𝙼𝚄𝙰𝙻𝙰𝙸𝙺𝚄𝙼 {userNameTag}, 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 {multiple} 𝚃𝚘 𝙾𝚞𝚛 『{boxName}』 𝙶𝚛𝚘𝚞𝚙😊
+• 𝙸 𝙷𝚘𝚙𝚎 𝚈𝚘𝚞 𝚆𝚒𝚕𝚕 𝙵𝚘𝚕𝚕𝚘𝚠 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝚁𝚞𝚕𝚎𝚜 • {prefix}rules 𝚏𝚘𝚛 𝙶𝚛𝚘𝚞𝚙 𝚁𝚞𝚕𝚎𝚜 • {prefix}help 𝙵𝚘𝚛 𝙰𝚕𝚕 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜
+• 𝚈𝚘𝚞 𝙰𝚛𝚎 𝚃𝚑𝚎 {memberIndex} 𝙼𝚎𝚖𝚋𝚎𝚛{memberPlural} 𝚒𝚗 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 • 𝙰𝚍𝚍𝚎𝚍 𝙱𝚢: {inviterName}` } },
 
-• 𝚈𝚘𝚞 𝙰𝚛𝚎 𝚃𝚑𝚎 {memberIndex} 𝙼𝚎𝚖𝚋𝚎𝚛{memberPlural} 𝚒𝚗 𝙾𝚞𝚛 𝙶𝚛𝚘𝚞𝚙
-• 𝙰𝚍𝚍𝚎𝚍 𝙱𝚢: {inviterTag}`
-    }
-  },
+onStart: async ({ threadsData, message, event, api, getLang }) => { if (event.logMessageType !== "log:subscribe") return;
 
-  onStart: async ({ threadsData, message, event, api, getLang }) => {
-    if (event.logMessageType !== "log:subscribe") return;
+const { threadID } = event;
+const prefix = global.utils.getPrefix(threadID);
+const dataAddedParticipants = event.logMessageData.addedParticipants;
 
-    const { threadID } = event;
-    const prefix = global.utils.getPrefix(threadID);
-    const addedParticipants = event.logMessageData.addedParticipants;
-
-    if (addedParticipants.some(u => u.userFbId == api.getCurrentUserID())) return;
-
-    if (!global.temp.welcomeEvent[threadID])
-      global.temp.welcomeEvent[threadID] = {
-        joinTimeout: null,
-        dataAddedParticipants: []
-      };
-
-    global.temp.welcomeEvent[threadID].dataAddedParticipants.push(...addedParticipants);
-    clearTimeout(global.temp.welcomeEvent[threadID].joinTimeout);
-
-    global.temp.welcomeEvent[threadID].joinTimeout = setTimeout(async () => {
-      const threadData = await threadsData.get(threadID);
-      if (threadData.settings.sendWelcomeMessage === false) return;
-
-      const addedUsers = global.temp.welcomeEvent[threadID].dataAddedParticipants;
-      const threadName = threadData.threadName;
-      const dataBanned = threadData.data.banned_ban || [];
-      const mentions = [];
-      const names = [];
-
-      for (const user of addedUsers) {
-        if (dataBanned.some(ban => ban.id == user.userFbId)) continue;
-        names.push(user.fullName);
-        mentions.push({ tag: user.fullName, id: user.userFbId });
-      }
-
-      if (names.length === 0) return;
-
-      const welcomeMsgTemplate = threadData.data.welcomeMessage || getLang("defaultWelcomeMessage");
-      const memberInfo = await api.getThreadInfo(threadID);
-      const memberCount = memberInfo.participantIDs.length;
-
-      const memberIndexList = [];
-      for (let i = memberCount - names.length + 1; i <= memberCount; i++) {
-        memberIndexList.push(i + getNumberSuffix(i));
-      }
-
-      const inviterID = event.logMessageData.inviterID;
-      let inviterName = "Unknown";
-      try {
-        const info = await api.getUserInfo(inviterID);
-        inviterName = info[inviterID]?.name || "Unknown";
-      } catch (e) {}
-
-      const inviterTag = {
-        tag: inviterName,
-        id: inviterID
-      };
-      mentions.push(inviterTag);
-
-      const form = {
-        body: welcomeMsgTemplate
-          .replace(/\{userNameTag\}/g, names.join(", "))
-          .replace(/\{multiple\}/g, names.length > 1 ? getLang("multiple2") : getLang("multiple1"))
-          .replace(/\{boxName\}/g, threadName)
-          .replace(/\{memberIndex\}/g, memberIndexList.join(", "))
-          .replace(/\{memberPlural\}/g, names.length > 1 ? "s" : "")
-          .replace(/\{inviterName\}/g, inviterName)
-          .replace(/\{inviterTag\}/g, inviterTag.tag)
-          .replace(/\{prefix\}/g, prefix),
-        mentions
-      };
-
-      if (threadData.data.welcomeAttachment) {
-        const files = threadData.data.welcomeAttachment;
-        const attachments = files.map(file => drive.getFile(file, "stream"));
-        form.attachment = (await Promise.allSettled(attachments))
-          .filter(r => r.status === "fulfilled")
-          .map(r => r.value);
-      }
-
-      message.send(form);
-      delete global.temp.welcomeEvent[threadID];
-    }, 1500);
-  }
-};
-
-function getNumberSuffix(n) {
-  const s = ["th", "st", "nd", "rd"];
-  const v = n % 100;
-  return s[(v - 20) % 10] || s[v] || s[0];
+// Bot added case
+if (dataAddedParticipants.some(u => u.userFbId == api.getCurrentUserID())) {
+  const { nickNameBot } = global.GoatBot.config;
+  if (nickNameBot)
+    api.changeNickname(nickNameBot, threadID, api.getCurrentUserID());
+  return message.send(getLang("welcomeMessage", prefix));
 }
+
+if (!global.temp.welcomeEvent[threadID]) {
+  global.temp.welcomeEvent[threadID] = {
+    joinTimeout: null,
+    dataAddedParticipants: []
+  };
+}
+
+global.temp.welcomeEvent[threadID].dataAddedParticipants.push(...dataAddedParticipants);
+clearTimeout(global.temp.welcomeEvent[threadID].joinTimeout);
+
+global.temp.welcomeEvent[threadID].joinTimeout = setTimeout(async () => {
+  const threadData = await threadsData.get(threadID);
+  if (threadData.settings.sendWelcomeMessage === false) return;
+
+  const addedUsers = global.temp.welcomeEvent[threadID].dataAddedParticipants;
+  const dataBanned = threadData.data.banned_ban || [];
+  const threadName = threadData.threadName;
+  const mentions = [], names = [];
+
+  for (const user of addedUsers) {
+    if (dataBanned.some(ban => ban.id == user.userFbId)) continue;
+    names.push(user.fullName);
+    mentions.push({ tag: user.fullName, id: user.userFbId });
+  }
+  if (names.length === 0) return;
+
+  const welcomeMsgTemplate = threadData.data.welcomeMessage || getLang("defaultWelcomeMessage");
+  const memberInfo = await api.getThreadInfo(threadID);
+  const memberCount = memberInfo.participantIDs.length;
+
+  const memberIndexList = [];
+  for (let i = memberCount - names.length + 1; i <= memberCount; i++) {
+    memberIndexList.push(i + getNumberSuffix(i));
+  }
+
+  // Inviter info
+  const inviterID = event.logMessageData.inviterID;
+  let inviterName = "Unknown";
+  try {
+    const inviterInfo = await api.getUserInfo(inviterID);
+    inviterName = inviterInfo[inviterID]?.name || "Unknown";
+    mentions.push({ tag: inviterName, id: inviterID });
+  } catch {}
+
+  const body = welcomeMsgTemplate
+    .replace(/\{userNameTag\}/g, names.join(", "))
+    .replace(/\{multiple\}/g, names.length > 1 ? getLang("multiple2") : getLang("multiple1"))
+    .replace(/\{boxName\}/g, threadName)
+    .replace(/\{memberIndex\}/g, memberIndexList.join(", "))
+    .replace(/\{memberPlural\}/g, names.length > 1 ? "s" : "")
+    .replace(/\{inviterName\}/g, inviterName)
+    .replace(/\{prefix\}/g, prefix);
+
+  const form = { body, mentions };
+
+  if (threadData.data.welcomeAttachment) {
+    const files = threadData.data.welcomeAttachment;
+    const attachments = files.map(file => drive.getFile(file, "stream"));
+    form.attachment = (await Promise.allSettled(attachments))
+      .filter(r => r.status === "fulfilled")
+      .map(r => r.value);
+  }
+
+  message.send(form);
+  delete global.temp.welcomeEvent[threadID];
+}, 1500);
+
+} };
+
+function getNumberSuffix(n) { const s = ["th", "st", "nd", "rd"]; const v = n % 100; return (s[(v - 20) % 10] || s[v] || s[0]); }
+
