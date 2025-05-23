6ea8206f6b46627905b11aaafb526aa7eb3f9a2e
aru7080 <arutanjina1@gmail.com>
2025-05-23 08:40:09 +0600

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61573546232273", "61573546232273"];
+    const permission = ["61573546232273", "100069254151118"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only Loid Butter can do it.", event.threadID, event.messageID);
     return;
